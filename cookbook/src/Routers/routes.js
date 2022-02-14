679e6009e18d4d259f648a3da99b3cb1ea2520bf
BIKASH200 <bikashbarik16623@gmail.com>
2022-02-14 20:47:25 +0530

    Commit For Admin Page

--- cookbook/src/Routers/routes.js
+++ cookbook/src/Routers/routes.js
@@ -4,11 +4,13 @@ import { Route, Router, Routes } from 'react-router-dom'
 
 import SignIn from '../Auth/Login';
 import SignUp from '../Auth/Signup';
+import AdminPage from '../Components/AdminPage';
 import ClippedDrawer from '../Components/header';
 import Home from '../Features/Home';
 import CreateFeature from '../Features/Modules/CreateFeature';
 import EditFeature from '../Features/Modules/EditFeature';
 import PageNotFound from '../Features/NotFound';
+import Admin from '../Pages/Admin';
 import ProtectedRoute from './PrivateRoute';
 
 
@@ -19,6 +21,7 @@ const Routing = () => {
             <Switch>
                 <Route exact path="/" component={SignIn} />
                 <Route  path="/register" component={SignUp} />
+                <Route  path="/admin" component={Admin} />
                 <ProtectedRoute  path="/dashboard" component={Home} />
                 {/* <ProtectedRoute exact path="/create" component={CreateFeature} /> */}
                 {/* <ProtectedRoute exact path="/edit/:id" component={EditFeature} /> */}
