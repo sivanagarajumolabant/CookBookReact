04132bcced988bb4fb853bdbc25086b6abad2823
siva <siva.n@quadrantresource.com>
2022-02-25 08:46:24 +0530

    chnages

--- cookbook/src/Config/config.js
+++ cookbook/src/Config/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = ()=>  "http://localhost:8000";
+const API_BASE_URL = ()=>  "http://20.47.89.104:81";
 // const API_BASE_URL = ()=>  "http://3.140.247.61";
 const ACCESS_TOKEN =()=> localStorage.getItem('quadranttoken');
  export default {API_BASE_URL, ACCESS_TOKEN}
